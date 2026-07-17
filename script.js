725ba083497ae53afa6c7d2f8a02ed4aba776c3e
StephanieGutie63 <stephanieg5@cpp.edu>
2026-07-16 19:08:52 -0700

    first and last name change

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ const message = document.getElementById("formMessage");
 form.addEventListener("submit", async function (e) {
     e.preventDefault();
 
-    const firstName = form.elements["fullName"].value.trim();
+    const firstName = form.elements["firstNameName"].value.trim();
+    const lastNameName = form.elements["lastName"].value.trim();
     const email = form.elements["email"].value.trim();
     const language = form.elements["language"].value;
     const age = form.elements["age"].value;
